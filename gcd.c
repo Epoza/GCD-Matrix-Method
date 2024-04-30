ff57a3113396f8e4016392d6311e0ea237d3a442
Epoza <86025026+Epoza@users.noreply.github.com>
2024-04-30 00:50:54 -0400

    add user input & modify variable names

--- gcd.c
+++ gcd.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 // Function declaration
-void calculate(int matrix[][3], int *gcd, int *topNum1, int *topNum2);
+void calculate(int matrix[][3], int *gcd, int *coefficient1, int *coefficient2);
 
 // Function to update the matrix
 void updateMatrix() {
@@ -10,7 +10,7 @@ void updateMatrix() {
 }
 
 // Function to print a matrix
-void printMatrix(int matrix[][3], int *gcd, int *topNum1, int *topNum2) {
+void printMatrix(int matrix[][3], int *gcd, int *coefficient1, int *coefficient2) {
     printf("\nYour new matrix is: \n");
 
     int rows = 2;
@@ -24,67 +24,79 @@ void printMatrix(int matrix[][3], int *gcd, int *topNum1, int *topNum2) {
         printf("\n");
     }
     // function to calculate next matrix
-    calculate(matrix, gcd, topNum1, topNum2);
+    calculate(matrix, gcd, coefficient1, coefficient2);
 }
 
 // Function to perform calculations on the matrix
-void calculate(int matrix[][3], int *gcd, int *topNum1, int *topNum2) {
-    int num1FromCalculate = matrix[0][0];
+void calculate(int matrix[][3], int *gcd, int *coefficient1, int *coefficient2) {
+    int num1 = matrix[0][0];
     int num2 = matrix[1][0];
-    int topNum1FromCalculate = matrix[0][1];
-    int topNum2FromCalculate = matrix[0][2];
+    int topNum1 = matrix[0][1];
+    int topNum2 = matrix[0][2];
     int botNum1 = matrix[1][1];
     int botNum2 = matrix[1][2];
     
     if (num2 == 0) {
-        printf("Calulations finished\n");
-        *gcd = num1FromCalculate;
-        *topNum1 = topNum1FromCalculate;
-        *topNum2 = topNum2FromCalculate;
+        printf("Calculations finished\n");
+        *gcd = num1;
+        *coefficient1 = topNum1;
+        *coefficient2 = topNum2;
         return;
     }
     
     int quotient;
-    if (num1FromCalculate >= num2) {
-        quotient = (num1FromCalculate / num2) * -1;
-        printf("%d goes into %d, %d times.\n", num2, num1FromCalculate, -1 * quotient);
+    if (num1 >= num2) {
+        quotient = (num1 / num2) * -1;
+        printf("%d goes into %d, %d times.\n", num2, num1, -1 * quotient);
         printf("Now multiply the bottom row by %d and add it to the top row.\n", quotient);
-        num1FromCalculate += (num2 * quotient);
-        topNum1FromCalculate += (botNum1 * quotient);
-        topNum2FromCalculate += (botNum2 * quotient);
-        printf("The numbers for the top row are now: %d %d %d\n", num1FromCalculate, topNum1FromCalculate, topNum2FromCalculate);
-        matrix[0][0] = num1FromCalculate;
-        matrix[0][1] = topNum1FromCalculate;
-        matrix[0][2] = topNum2FromCalculate;
-        printMatrix(matrix, gcd, topNum1, topNum2);
+        num1 += (num2 * quotient);
+        topNum1 += (botNum1 * quotient);
+        topNum2 += (botNum2 * quotient);
+        printf("The numbers for the top row are now: %d %d %d\n", num1, topNum1, topNum2);
+        matrix[0][0] = num1;
+        matrix[0][1] = topNum1;
+        matrix[0][2] = topNum2;
+        printMatrix(matrix, gcd, coefficient1, coefficient2);
     } else {
         // Does the opposite of the if statement
-        quotient = (num2 / num1FromCalculate) * -1;
-        printf("%d goes into %d, %d times.\n", num1FromCalculate, num2, -1 * quotient);
+        quotient = (num2 / num1) * -1;
+        printf("%d goes into %d, %d times.\n", num1, num2, -1 * quotient);
         printf("Now multiply the top row by %d and add it to the bottom row.\n", quotient);
-        num2 += (num1FromCalculate * quotient);
-        botNum1 += (topNum1FromCalculate * quotient);
-        botNum2 += (topNum2FromCalculate * quotient);
+        num2 += (num1 * quotient);
+        botNum1 += (topNum1 * quotient);
+        botNum2 += (topNum2 * quotient);
         printf("The numbers for the bottom row are now: %d %d %d\n", num2, botNum1, botNum2);
         matrix[1][0] = num2;
         matrix[1][1] = botNum1;
         matrix[1][2] = botNum2;
-        printMatrix(matrix, gcd, topNum1, topNum2);
+        printMatrix(matrix, gcd, coefficient1, coefficient2);
     }
 }
 
 int main() {
-    // Eventually get user input for both numbers
-    int input1 = 129;
-    int input2 = 60;
+    int input1, input2;
+
+    // Get user input for both numbers
+    printf("Enter the first number: ");
+    scanf("%d", &input1);
+    printf("Enter the second number: ");
+    scanf("%d", &input2);
+
+    // Check and swap if necessary so that input1 stores the higher number
+    if (input2 > input1) {
+        int temp = input1;
+        input1 = input2;
+        input2 = temp;
+    }
+
     int baseMatrix[2][3] = {{input1, 1, 0},
                             {input2, 0, 1}};
-                            
+
     // Variables to store values returned from calculate
     int gcd, coefficient1, coefficient2;
 
     // Call the function to print the matrix
-    printMatrix(baseMatrix, &gcd, &coefficient1, &coefficient2); // 2 rows, 3 columns
+    printMatrix(baseMatrix, &gcd, &coefficient1, &coefficient2);
 
     // Display the results
     printf("\nThe GCD is: %d\n", gcd);
